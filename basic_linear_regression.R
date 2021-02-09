a0a0f8d28e66a1be0ad5cb82dc6291b187f1359c
stayukit <77545234+stayukit@users.noreply.github.com>
2021-02-09 23:10:47 +0700

    Update basic_linear_regression.R

--- basic_linear_regression.R
+++ basic_linear_regression.R
@@ -1,16 +1,16 @@
 # linear regression model, lm
 # binary linear regression: 1:1 y:x variable 
-imdb = read.csv("imdb.csv")
+imdb = read.csv("imdb.csv") # import dataset
 head(imdb)
 # score = f(length)
-lmmodel <- lm(SCORE ~ LENGTH, data = imdb)
+lmModel <- lm(SCORE ~ LENGTH, data = imdb)
 # SCORE, LENGTH from imdb data
 # score = 8.162965 + 0.002624*length
 # length = 120, score = ?
-coef(lmmodel)
+coef(lmModel) # can use index [0], [1] like python
 # (Intercept)      LENGTH 
 # 8.162964914 0.002624016 
-coef(lmmodel)[1] # show intercept
-coef(lmmodel)[2] # show length
+coef(lmModel)[1] # show intercept
+coef(lmModel)[2] # show length
 
-coef(lmmodel)[1] + coef(lmmodel)[2]*120
+coef(lmModel)[1] + coef(lmModel)[2]*120
